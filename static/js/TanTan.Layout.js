d0d2b586dd66380bf1c83fa68f9dd8fe97cf2f4f
Joaquin R <globojorro@gmail.com>
2013-10-02 00:00:35 -0500

    Definir la región de navegación y usar plantilla en #appnav

---
+++ static/js/TanTan.Layout.js
@@ -0,0 +1,14 @@
+/*global TodoMVC */
+'use strict';
+
+TanTan.module('Layout', function (Layout, App, Backbone) {
+    // Navbar Layout 
+    // ------------------
+    Layout.Nav = Backbone.Marionette.Layout.extend({
+        template: '#appnav',
+        regions: {
+            menu: '#nav-menu',
+            actions: '#nav-actions'
+        }
+    });
+});
