c50961b1e74aff67b0ef6289f05b29326f668136
Joaquin R <globojorro@gmail.com>
2013-10-20 00:25:19 -0500

    Definir modelo para usuarios

--- static/js/TanTan.Models.js
+++ static/js/TanTan.Models.js
@@ -5,4 +5,20 @@ TanTan.module('Models', function(Models, App, Backbone) {
 
     Models.Base = Backbone.Model.extend({ idAttribute: '_id' });
 
+    Models.UserDoc = Models.Base.extend({
+        defaults: {
+            name: '',
+            type: 'user',
+            roles: []
+        },
+        validate: function (attribs) {
+            if (!attr.name) {
+                return 'Name needed'
+            }
+            if (!attr._id) {
+                return 'ID Needed'
+            }
+        }
+    });
+
 });
