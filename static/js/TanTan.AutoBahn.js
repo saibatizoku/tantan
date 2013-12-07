cb7793c55463319b013f1a707d978257b246ef02
Joaquin R <globojorro@gmail.com>
2013-12-06 23:37:39 -0600

    Añadir login a AutoBahn

--- static/js/TanTan.AutoBahn.js
+++ static/js/TanTan.AutoBahn.js
@@ -19,9 +19,23 @@ TanTan.module('AutoBahn', function (AutoBahn, App, Backbone, Marionette, $, _) {
         App.vent.trigger('wamp:failure');
     }
 
+    function doLogin (resp) {
+        if (resp._id) {
+            ab.log('good login', resp);
+            App.vent.trigger('granjas:loggedIn', resp);
+        } else {
+            ab.log('bad login', resp);
+            App.vent.trigger('granjas:loggedOut', resp);
+        }
+    }
+
     AutoBahn.connect = function () {
         sess = new ab.Session(this.wsuri, onConnect, onHangup);
         ab.log('AutoBahn session', sess);
+    };
+
+    AutoBahn.login = function (creds) {
+        sess.call('rpc:login', creds).always(doLogin);
     };
 
 });
