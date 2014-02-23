c8c2a787084abe7e2229d169db7f81bf715b854a
Joaquin R <globojorro@gmail.com>
2014-02-23 11:12:36 -0600

    Código ZB sin uso

--- zb.py
+++ zb.py
@@ -130,7 +130,6 @@ class TantanZB(txXBee):
             return Nodo('')
 
     def handle_packet(self, packet):
-        msg = None
         if self.extractNodeInfo(packet):
             if self.is_RX(packet):
                 self.handle_rx(packet)
@@ -191,15 +190,12 @@ class TantanZB(txXBee):
                            'sensor': sensor,
                            'value': float(value),
                            }
-                #print reading
                 self.wsMcuFactory.dispatch("http://www.tantan.org/api/sensores#amb-rx", reading)
                 uri = "/".join(["http://www.tantan.org/api/sensores/nodos#", node_id])
                 self.wsMcuFactory.dispatch(uri, {'node_id': node_id, 'msg': l})
-                #self.wsMcuFactory.dispatch("http://www.tantan.org/api/couchdb#info", reading)
             except:
                 uri = "/".join(["http://www.tantan.org/api/sensores#", node_id])
                 self.wsMcuFactory.dispatch(uri, {'node_id': node_id, 'msg': l})
-        #self.wsMcuFactory.dispatch("http://www.tantan.org/api/sensores#zb-rx", evt)
 
     @exportRpc("send-nd")
     def sendND(self, evt=None):
@@ -225,14 +221,12 @@ class TantanZB(txXBee):
         elif "source_addr_long" in packet["parameter"]:
             laddr = packet["parameter"]["source_addr_long"].encode('hex')
         
-        #nname = ZB_reverse.get(laddr, "Unknown")
         nname = packet["parameter"]["node_identifier"]
         addr = packet["parameter"]["source_addr"].encode('hex')
         devt = packet["parameter"]["device_type"].encode('hex')
         devs = packet["parameter"]["status"].encode('hex')
         paddr = packet["parameter"]["parent_address"].encode('hex')
         msg = ":".join([nname, addr, "ND", laddr, devt, devs, paddr]) #, str(packet)])
-        #print msg
         device.update({'id': laddr,
             'name': nname,
             'value': {'long': laddr,
@@ -243,7 +237,6 @@ class TantanZB(txXBee):
                 'status': devs,
                 }
             })
-        #print device
         pulse = []
         if laddr in self.devices:
             pulse = self.devices[laddr]['pulse']
@@ -252,138 +245,11 @@ class TantanZB(txXBee):
         pulse.insert(0, beat)
         device.update({'pulse': pulse})
         self.devices[laddr] = device
-        #print "heartbeat:", nname, beat
 
         self.wsMcuFactory.dispatch("http://www.tantan.org/api/sensores#zb-nd", device)
-        #self.wsMcuFactory.dispatch("http://www.tantan.org/api/couchdb#info", device)
-
-
-class UnUsed:
-    def handle_badpacket(self, packet):
-        print "ERROR:", packet
-
-
-    def handle_db(self, packet):
-        topic = "http://www.tantan.org/api/sensores#zb-db"
-        utils.handle_factory_db(self.wsMcuFactory, packet, topic)
-
-    def handle_packet(self, xbeePacketDictionary):
-        response = xbeePacketDictionary
-        msg = None
-        print repr(self._frame.raw_data.encode('hex'))
-        print repr(response)
-        #if response.get("source_addr_long", "default") in ZB_reverse:
-        if response.get("id", "default") == "rx":
-            self.handle_rx(response)
-            #broadcastToClients(response, msg)
-        elif response.get("status", "default") == "\x00":
-            if response.get("command", "default") == "ND":
-                self.handle_nd(response)
-            elif response.get("command", "default") == "DB":
-                self.handle_db(response)
-            elif response.get("command", "default") == "%V":
-                if response.get("id", "default") == "remote_at_response":
-                    nname = ZB_reverse.get(response["source_addr_long"], "Unknown")
-                    laddr = response["source_addr_long"].encode('hex')
-                    addr = response["source_addr"].encode('hex')
-                    val = 0
-                    if "parameter" in response:
-                        val = int(response["parameter"].encode('hex'),16) * 1200.0 / 1024 / 1000
-                    msg = ":".join([nname, addr, "%V", str(val)])
-                    evt = {'id': nname, 'value': val}
-                    self.wsMcuFactory.dispatch("http://www.tantan.org/api/sensores#zb-volt", evt)
-                else:
-                    val = int(response["parameter"].encode('hex'),16) * 1200.0 / 1024 / 1000
-                    msg = "C0 %V:", str(val), "[mV]"
-                    evt = {'id': 'C0', 'value': val}
-                    self.wsMcuFactory.dispatch("http://www.tantan.org/api/sensores#zb-volt", evt)
-                    
-        elif 'samples' in response:
-            #print strftime("%Y-%m-%d %H:%M:%S").encode('utf8'), "<<< FROM:", response
-            nname = ZB_reverse.get(response["source_addr_long"], "Unknown")
-            laddr = response["source_addr_long"].encode('hex')
-            addr = response["source_addr"].encode('hex')
-            val = str(dict((str(key).replace('-',''), str(value)) for (key, value) in response["samples"][0].items())).replace("'",'"')
-            msg = ":".join([nname, addr, "RXIO", val])
-
-        broadcastToClients(response, msg)
-
-        #if response.get("source_addr_long", "default") == devices["template1"]:
-        #    reactor.callFromThread(self.send,
-        #            "tx",
-        #            frame_id="\x01",
-        #            dest_addr_long=devices["template2"],
-        #            dest_addr="\xff\xfe",
-        #            data="DATA2")
-
-    @exportRpc("control-led")
-    def controlLed(self, evt=None):
-        if evt:
-            if evt == "RXTX":
-                reactor.callFromThread(self.send,
-                    "remote_at",
-                    frame_id="\x02",
-                    dest_addr_long="\x00\x00\x00\x00\x00\x00\xff\xff",
-                    dest_addr="\xff\xfe",
-                    command="D5",
-                    parameter="\x01"
-                    )
-            if evt == "ON":
-                reactor.callFromThread(self.send,
-                    "remote_at",
-                    frame_id="\x02",
-                    dest_addr_long="\x00\x00\x00\x00\x00\x00\xff\xff",
-                    dest_addr="\xff\xfe",
-                    command="D5",
-                    parameter="\x05"
-                    )
-            if evt == "OFF":
-                reactor.callFromThread(self.send,
-                    "remote_at",
-                    frame_id="\x02",
-                    dest_addr_long="\x00\x00\x00\x00\x00\x00\xff\xff",
-                    dest_addr="\xff\xfe",
-                    command="D5",
-                    parameter="\x04"
-                    )
-            return 'ASSOC LED Sent: {0}'.format(evt)
-
-    @exportRpc("send-tx")
-    def sendTX(self, evt=None):
-        #self._sendTX()
-        print evt
-        return 'TX Sent'
-        
-    def _sendTX(self):
-        reactor.callFromThread(self.send,
-                "tx",
-                frame_id="\x01",
-                command="ND",
-                )
-    @exportRpc("send-dbvolt")
-    def sendDB_Volt(self, evt=None):
-        self._sendDB_Volt()
-        return "DBVOLT Sent"
-    
-    def _sendDB_Volt(self):
-        reactor.callFromThread(self.send,
-                "remote_at",
-                frame_id="\x02",
-                dest_addr_long="\x00\x00\x00\x00\x00\x00\xff\xff",
-                dest_addr="\xff\xfe",
-                command="DB",
-                )
-        reactor.callFromThread(self.send,
-                "remote_at",
-                frame_id="\x03",
-                dest_addr_long="\x00\x00\x00\x00\x00\x00\xff\xff",
-                dest_addr="\xff\xfe",
-                command="%V",
-                )
 
 
 ## WS-MCU protocol
-##
 class WsMcuProtocol(WampServerProtocol):
  
     def onSessionOpen(self):
@@ -393,7 +259,6 @@ class WsMcuProtocol(WampServerProtocol):
 
 
 ## WS-MCU factory
-##
 class WsMcuFactory(WampServerFactory):
 
     protocol = WsMcuProtocol
@@ -401,8 +266,6 @@ class WsMcuFactory(WampServerFactory):
     def __init__(self, url, opts=None):
         WampServerFactory.__init__(self, url)
         self.zbProtocol = TantanZB(wsMcuFactory=self)
-        #self.serialport = SerialPort(self.zbProtocol, opts['port'], reactor, baudrate=opts['baudrate'])
-
 
 
 if __name__ == '__main__':
@@ -429,7 +292,6 @@ if __name__ == '__main__':
     listenWS(wsMcuFactory)
 
     ## create embedded web server for static files
-    ##
     webdir = File("./static")
     web = Site(webdir)
     reactor.listenTCP(webport, web)
