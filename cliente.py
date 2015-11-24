685e8eb13b9a8c2cdf50774f52bfe75aed236dcd
Joaquin R <globojorro@gmail.com>
2015-11-24 12:04:10 -0600

    limpiar código

--- cliente.py
+++ cliente.py
@@ -15,30 +15,33 @@
 ##  limitations under the License.
 ##
 ###############################################################################
-
-
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.serialport import SerialPort
 
-from txXBee.protocol import txXBee
-
 from autobahn.twisted.wamp import ApplicationSession
+from txXBee.protocol import txXBee
 
 from handlers import handle_rx
 from handlers import is_RX
 from handlers import is_PANID
 
-class McuProtocol(txXBee):
 
+
+class McuProtocol(txXBee):
+    """ Protocol for communicating with a ZigBee network through a
+        coordinator node in API Mode.
+
+        Default values include :escaped = True for ZigBee API=2 mode
+    """
     def __init__(self, session, debug = False, escaped=True):
+        """ Default values include :escaped = True for ZigBee API=2 mode
+        """
         super(McuProtocol, self).__init__(escaped=escaped)
         self.debug = debug
         self.session = session
         self.pan_id = None
 
     def connectionMade(self):
-        #pans = self.factory.getNetworkInfo()
-        #self.factory.resetDelay()
         self.getPanId()
 
     def handle_packet(self, packet):
@@ -50,23 +53,8 @@ class McuProtocol(txXBee):
             handle_rx(self, packet)
 
     def connectionLost(self, reason):
-        print "AGENT connection LOST" #, self.factory.pan_id, self.factory.service.pan.keys()
-        #pans = self.factory.service.pan
-        #if self.factory.pan_id in pans:
-        #    pans[self.factory.pan_id].protocol.transport.loseConnection()
+        print "AGENT connection LOST"
 
-
-    def controlLed(self, turnOn):
-        """
-        This method is exported as RPC and can be called by connected clients
-        """
-        if turnOn:
-            payload = b'1'
-        else:
-            payload = b'0'
-        if self.debug:
-            print("Serial TX: {0}".format(payload))
-        self.transport.write(payload)
 
     def getPanId(self):
         reactor.callFromThread(self.send,
@@ -96,7 +84,6 @@ class McuComponent(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
         print("MyComponent ready! Configuration: {}".format(self.config.extra))
-
         port = self.config.extra['port']
         baudrate = self.config.extra['baudrate']
         debug = self.config.extra['debug']
@@ -123,17 +110,23 @@ if __name__ == '__main__':
     parser.add_argument("-d", "--debug", action = "store_true",
             help = "Enable debug output.")
 
-    parser.add_argument("--baudrate", type = int, default = 9600, choices = [300, 1200, 2400, 4800, 9600, 19200, 57600, 115200],
+    parser.add_argument("--baudrate", type = int, default = 9600,
+            choices = [300, 1200, 2400, 4800, 9600, 19200, 57600, 115200],
             help = 'Serial port baudrate.')
 
     parser.add_argument("--port", type = str, default = '/dev/ttyACM0',
-            help = 'Serial port to use (e.g. 3 for a COM port on Windows, /dev/ttyATH0 for Arduino Yun, /dev/ttyACM0 for Serial-over-USB on RaspberryPi.')
+            help = 'Serial port to use (e.g. 3 for a COM port on Windows, \
+                    /dev/ttyATH0 for Arduino Yun, /dev/ttyACM0 for \
+                    Serial-over-USB on RaspberryPi.')
 
     parser.add_argument("--web", type = int, default = 0,
             help = 'Web port to use for embedded Web server. Use 0 to disable.')
 
-    parser.add_argument("--router", type = str, default = 'wss://ruta-energetica-2014.herokuapp.com/ws',
-            help = 'If given, connect to this WAMP router. Else run an embedded router on 8080.')
+    parser.add_argument("--router", type = str,
+            default = 'wss://ruta-energetica-2014.herokuapp.com/ws',
+            help = 'If given, connect to this WAMP router. Else run an \
+                    embedded router on 8080. Default: \
+                    wss://ruta-energetica-2014.herokuapp.com/ws')
 
     args = parser.parse_args()
 
@@ -149,22 +142,17 @@ if __name__ == '__main__':
 
     ## import Twisted reactor
     if sys.platform == 'win32':
-        ## on windows, we need to use the following reactor for serial support
-        ## http://twistedmatrix.com/trac/ticket/3802
-        ##
         from twisted.internet import win32eventreactor
         win32eventreactor.install()
 
     from twisted.internet import reactor
     print("Using Twisted reactor {0}".format(reactor.__class__))
 
-
     ## create embedded web server for static files
     if args.web:
         from twisted.web.server import Site
         from twisted.web.static import File
         reactor.listenTCP(args.web, Site(File(".")))
-
 
     ## run WAMP application component
     from autobahn.twisted.wamp import ApplicationRunner
--- handlers.py
+++ handlers.py
@@ -20,36 +20,41 @@
 
 def handle_rx(self, packet):
     resp = {}
-    #resp['name'] = ZB_reverse.get(packet["source_addr_long"], "Unknown")
     resp = get_zb_node_info(self, packet)
     print "pak: {}".format(repr(packet))
     rout = "RX:"
+
     for (key, item) in resp.items():
         rout += "{0}-{1}:".format(key, item)
     msg = "{0}:{1}:RX:".format(resp['id'], resp['addr'], resp['data']) + repr(resp['data'])
-    #print 'Evt id: {0}\nVal: {1}'.format(str(resp['name']), resp['data'].decode('utf8'))
+
     evt = {'id': resp['id'],
             'type': 'rx',
             'data': resp['data'],
             }
+
     node_id = resp['id']
     data = resp['data']
     data_lines = data.splitlines()
 
     for l in data_lines:
         topic_id = u"mx.neutro.energia.api.nodos"
-        topic = u".".join(["mx.neutro.energia.api.nodos", node_id])
-        vals = V1, PA1, FP1, P1, P2, P3, P4 = [float(val) for val in l.rstrip(' \0').split() ]
-        sens_k = [ 'v1', 'pa1', 'fp1', 'p1', 'p2', 'p3', 'p4' ]
-        sensores = dict(zip(sens_k, vals))
-#print topic_id, repr(vals), sensores
-        if self.pan_id:
-            topic_pan_id = u"mx.neutro.energia.api.redes"
-            print topic_pan_id, self.pan_id, topic_id, node_id, sensores
-            self.session.publish(topic_pan_id, {'pan_id': self.pan_id, 'node_id': node_id})
-            self.session.publish(topic_id, {'pan_id': self.pan_id, 'node_id': node_id, 'msg': vals, 'sensores': sensores})
-        else:
-            self.getPanId()
+        topic_nodo = u".".join(["mx.neutro.energia.api.nodos", node_id])
+        topic_pan_id = u"mx.neutro.energia.api.redes"
+        try:
+            vals = V1, V2, V3, V4, I1, I2, I3, I4, P1, P2, P3, P4, CMD, EST, SMF = [float(val) for val in l.rstrip(' \0').split() ]
+            sens_k = [ 'v1', 'v2', 'v3', 'v4', 'i1', 'i2', 'i3', 'i4', 'p1', 'p2', 'p3', 'p4', 'cmd', 'est', 'smf' ]
+            sensores = dict(zip(sens_k, vals))
+            print "Publicando a:\n\t{}\n\t{}\n\t{}".format(topic_pan_id, topic_id, topic_nodo)
+            if self.pan_id:
+                print "PUBLICANDO PANID"
+                self.session.publish(topic_pan_id, {'pan_id': self.pan_id, 'node_id': node_id})
+                self.session.publish(topic_id, {'pan_id': self.pan_id, 'node_id': node_id, 'msg': vals, 'sensores': sensores})
+                self.session.publish(topic_nodo, {'pan_id': self.pan_id, 'node_id': node_id, 'msg': vals, 'sensores': sensores})
+            else:
+                self.getPanId()
+        except:
+            print "FORMATO MALO"
 
 def is_target_type(self, packet, field, target):
     if field in packet and packet[field].lower() == target:
@@ -77,4 +82,3 @@ def get_zb_node_info(self, packet):
         return resp
     except:
         return None
-
